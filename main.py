7e8d5b740f05a74960f5e2755edfb629adf43246
hoangks5 <hoangnd.ute@gmail.com>
2023-03-03 05:55:19 +0000

    update write log

--- main.py
+++ main.py
@@ -5,12 +5,28 @@ import time
 import requests
 import json
 import logging
-logging.basicConfig(filename='info.log',filemode='w',level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
+import pymongo
+import certifi
+
+# Cấu hình logging cho ứng dụng của bạn
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+
+# Tạo một đối tượng handler để ghi tiếp vào file log
+handler = logging.FileHandler('example.log')
+handler.setLevel(logging.DEBUG)
+
+# Thêm handler vào logger
+logger = logging.getLogger()
+logger.addHandler(handler)
 
 API_KEY_ETHERSCAN = os.getenv('API_KEY_ETHERSCAN')
 
-def call_requests_api_etherscan(start_block,end_block):
+client = pymongo.MongoClient("mongodb+srv://hoangks5:YrfvDz4Mt8xrrHxi@cluster0.tcbxc.mongodb.net/",tlsCAFile=certifi.where())
+database = client['nft']
+collection = database['etherscan']
 
+def call_requests_api_etherscan(start_block,end_block):
+    global collection
     urlAPI = 'https://api.etherscan.io/api'
     params = {
                     "module" : "logs",
@@ -39,27 +55,20 @@ def call_requests_api_etherscan(start_block,end_block):
             if len(obj['topics']) != 4:
                 continue
             transaction_ERC_721.append(obj)
-    
-    logging.info('Block: '+str(start_block)+' To '+str(end_block) + ', Step block: '+str(end_block-start_block)+ ', Total transaction: '+str(len(transaction_number))+', Transaction ERC-721: '+str(len(transaction_ERC_721)))
-    
-    
-    
-    
-    
-    
-    
-    
+            if obj['transactionHash'] not in list(collection.distinct('transactionHash')):
+                collection.insert_one(obj)
+
+    logger.info('Block: '+str(start_block)+' To '+str(end_block) + ', Step block: '+str(end_block-start_block)+ ', Total transaction: '+str(len(transaction_number))+', Transaction ERC-721: '+str(len(transaction_ERC_721)))
     return result
+
 
 def main():
     start_block = 1
     step_block = 1
 
     result = call_requests_api_etherscan(start_block=start_block,end_block=start_block+step_block)
-    for i in range(50):
+    for i in range(2000):
         if result['status'] == '0':
-            transaction_number = []
-            transaction_ERC_721 = []
             start_block += step_block
             step_block *=2
             result = call_requests_api_etherscan(start_block=start_block,end_block=start_block+step_block)
@@ -68,30 +77,18 @@ def main():
             # Total transaction
             transaction_number = len(result['result'])
             
-            
-            # Filter transaction ERC 721
-            transaction_ERC_721 = []
-            for obj in result['result']:
-                if len(obj['topics']) != 4:
-                    continue
-                transaction_ERC_721.append(obj)
-                
-            
             if transaction_number <= 4000:
-                
                 start_block = start_block + step_block
                 step_block *= 2
                 result = call_requests_api_etherscan(start_block=start_block,end_block=start_block+step_block)
                 continue
 
             if transaction_number == 10000:
-
                 step_block = int(step_block/2)
                 result = call_requests_api_etherscan(start_block=start_block,end_block=start_block+step_block)
                 continue
                 
             else:
-
                 start_block += step_block
                 result = call_requests_api_etherscan(start_block=start_block,end_block=start_block+step_block)
                 continue
